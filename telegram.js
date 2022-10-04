47595090cc18296e4193708ce64e0b417c92c9a1
Lamhot Simamora <lamhotsimamora36@gmail.com>
2022-10-04 18:34:24 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,4 +1,4 @@
-const bot_key="1888710721:AAE9QXln5ah4kidcVkAIQGU-Bh4smeF2lhQ"
+const bot_key="5786170846:AAFVwiakMkWTvq1eGnI0iYh3xhQfD2iJWB8"
 
 const _URL_ = "https://api.telegram.org/bot"+bot_key+"/sendMessage?chat_id=630610100&text="
 
